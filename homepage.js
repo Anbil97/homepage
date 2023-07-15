fba01f498bc891cb19dda6e7753d0d927a7d2de8
Anbil97 <126085127+Anbil97@users.noreply.github.com>
2023-07-15 19:52:38 +0530

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Shop Homepage</h1>
+      {/* Add your shop content here */}
+    </div>
+  );
+};
+
+export default HomePage;
